d2c5ca95a8b2076e1a32c0066b75f43870d0e596
John McK <john.mckech3@gmail.com>
2020-06-19 17:39:40 -0500

    adding questions

---
+++ assets/javascript.js
@@ -0,0 +1,60 @@
+var quizDiv = document.getElementById('quiz');
+var resultsDiv = document.getElementById('results')
+
+
+var questionArray = [{
+    q: "Who won the NFL MVP in 2019?",
+    answers: {
+        a: "Dak Prescott",
+        b: "Lamar Jackson",
+        c: "Nick Chubb",
+        d: "Patrick Mahomes"
+    },
+    correctAnswer: "b"
+},
+{
+    q: "Who won the Heisman Trophy in 2019",
+    answers: {
+        a: "Kyler Murray",
+        b: "Justin Herbert",
+        c: "Joe Burrow",
+        d: "Tua Tagovailoa"
+    },
+    correctAnswer: "c"
+},
+{
+    q: "Who won the most recent Super Bowl?",
+    answers: {
+        a:"San Francisco 49ers",
+        b:"Kansas City Chiefs",
+        c:"Denver Broncos",
+        d:"Philadelphia Eagles",
+    },
+    correctAnswer: "b"
+},
+{
+    q: "Who won the college football national championship?",
+    answers: {
+        a:"Alabama",
+        b:"Clemson",
+        c:"Wisconsin",
+        d:"LSU",
+    },
+    correctAnswer: "d"
+},
+]
+
+function buildQuiz(){}
+
+
+var score = 0
+    
+
+function showResults() {
+    
+}
+
+for (var i = 0; i< array.length; index++) {
+    var userInput  = array[index];
+    
+}
